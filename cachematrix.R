55e5975a10f1a6fe85c67dab25f8ebf54a6d550a
prosen2 <prosen2@gmail.com>
2014-04-21 20:22:21 -0500

    Initial Commit
    Entered initial comment for project

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will take a matrix as an argument and return the inverse of that matrix
+## The function will first check to see if the inverse matrix has already been cached
+## If the inverse already exists in the cache, the function will read and return it from the cache
+## If the inverse is not in the cache, the function will calculate the inverse and store it
+## in the cache. 
 
 ## Write a short comment describing this function
 
